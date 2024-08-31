1d2d3fbdf6d7e09c6f756215b995b2a632f0473a
Jesus Soto <sotous0@gmail.com>
2024-08-31 16:18:15 -0500

    Initial commit

---
+++ src/app/location/page.tsx
@@ -0,0 +1,2 @@
+const Page = () => <h3>Displayed city</h3>;
+export default Page;
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    '/src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      }
+    }
+  },
+  plugins: [],
+}
+
