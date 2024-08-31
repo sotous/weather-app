4bc32c58495ac4c6a2f08b184302ac7c0046e0cd
Jesus Soto <sotous0@gmail.com>
2024-08-31 17:05:12 -0500

    Revert themes in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,22 @@ const config: Config = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      }
+    }
   },
   plugins: [],
 }
