4e6f7542976b6a6da78e2a5832bf1100e9b2a8bf
Jesus Soto <sotous0@gmail.com>
2024-09-03 18:34:27 -0500

    Finish implementing all styles in tailwind

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
-        <div className="flex flex-col">
-            <div className="flex justify-between">
-                <Image src={"/assets/img/thermometer.svg"} width={15} height={15} alt="Thermometer"/>
-                <div>10</div>
+        <div className="flex flex-col pt-10 text-gray-400 text-[10px]">
+            <div className="flex justify-between items-center gap-x-2">
+                <Image src={"/assets/img/thermometer.svg"} width={12} height={12} alt="Thermometer"/>
+                <div>10°</div>
                 <div>/</div>
-                <div>23</div>
+                <div className="text-black text-base font-semibold">23°</div>
                 <div>/</div>
-                <div>25</div>
+                <div>25°</div>
             </div>
-            <div>
-                <p>4 mph, SE direction</p>
+            <div className="flex flex-row items-center">
+                <Image src={"/assets/img/wind.svg"} width={20} height={20} alt="Wind"/>
+                <p className="text-center">4 mph, SE direction</p>
             </div>
         </div>
     );
