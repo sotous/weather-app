b07eafd7dc7a5a38594e27caa73b61f0b400fbcb
Jesus Soto <sotous0@gmail.com>
2024-12-28 15:58:08 -0500

    Create the useSearch hook

---
+++ src/app/hooks/useSearch.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+export type Place = {
+  lat: string;
+  lon: string;
+  name: string;
+  display_name: string;
+};
+
+const useSearch = () => {
+  const url = `https://nominatim.openstreetmap.org/search`;
+  const getPlaces = async (query: string) => {
+    try {
+      const response = await fetch(`${url}?q=${query}&format=json`);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}, ${response.statusText}`);
+      }
+      const places = await response.json();
+      return places;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return { getPlaces };
+};
+export default useSearch;
