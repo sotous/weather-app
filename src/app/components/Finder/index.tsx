a9fb2b4b88548a9b1ffca5d07bb19bd311db8de2
Jesus Soto <sotous0@gmail.com>
2024-12-28 15:58:23 -0500

    Complete the finder feature

--- src/app/components/Finder/index.tsx
+++ src/app/components/Finder/index.tsx
@@ -1,9 +1,38 @@
+"use client";
+import { debounce } from "lodash";
+import { useState } from "react";
+
+import useSearch, { Place } from "@/app/hooks/useSearch";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
 const Finder = () => {
-    return (
-        <div className="p-[1.5rem]">
-            <input className="border-solid border-2 rounded-sm h-10 p-2" type="text" placeholder="Find a city" />
-        </div>
-    );
+  const { getPlaces } = useSearch();
+  const [debouncedCallApi] = useState(() => debounce(getPlaces, 300));
+  const [finderOptions, setFinderOptions] = useState<Place[]>([]);
+
+  const findPlaces = async (query: string) => {
+    console.log(query);
+    if (query !== "") {
+      const places = (await debouncedCallApi(query)) ?? [];
+      setFinderOptions(places);
+      console.log(places);
+    }
+  };
+  return (
+    <div className="p-[1.5rem]">
+      <Autocomplete
+        freeSolo
+        onKeyUp={(e: React.SyntheticEvent) => {
+          findPlaces((e.target as HTMLInputElement).value);
+        }}
+        options={finderOptions}
+        getOptionLabel={(option) => (option as Place).display_name}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Find places..." />}
+      />
+    </div>
+  );
 };
 
 export default Finder;
