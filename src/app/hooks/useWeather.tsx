d7b8b4eb55a6f7cf1cd9031e5ca6bc39b0dc1958
Jesus Soto <sotous0@gmail.com>
2025-01-06 14:23:00 -0500

    Create the useWeather hook

---
+++ src/app/hooks/useWeather.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+export type WeatherForecast = {
+  current: {
+    temperature_2m: number;
+    precipitation: number;
+    rain: number;
+    showers: number;
+    snowfall: number;
+    wind_speed_10m: number;
+    wind_direction_10m: number;
+    cloud_cover: number;
+    is_day: number;
+    sunshine_duration: number;
+    lightning_potential: number | null;
+  };
+  apparent_temperature_max: number[];
+  apparent_temperature_min: number[];
+};
+export type CurrentConditions = {
+  minTemp: number;
+  maxTemp: number;
+  currentTemp: number;
+  windSpeed: number;
+  windDirection: string;
+  condition: string;
+};
+
+const useWeather = () => {
+  const url = ``;
+};
+export default useWeather;
