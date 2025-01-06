ac4430c009efed3e10f18950fc6806cad7a7bf92
Jesus Soto <sotous0@gmail.com>
2025-01-06 14:22:02 -0500

    Present data in heading index

--- src/app/components/Heading/index.tsx
+++ src/app/components/Heading/index.tsx
@@ -1,10 +1,21 @@
-const Heading = () => {
-    return (
-        <div className="pt-10 pb-[0.5rem]">
-            <h3 className="uppercase font-bold">Cupertino</h3>
-            <p className="uppercase text-xs text-slate-400 text-center">Light rain</p>
-        </div>
-    );
-}
+"use client";
+import { Dispatch, SetStateAction, useState } from "react";
+
+import { Place } from "@/app/hooks/useSearch";
+
+type HeadingProps = {
+  place: Place;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+};
+
+const Heading = ({ place, setLoading }: HeadingProps) => {
+  return (
+    <div className="pt-10 pb-[0.5rem] text-center">
+      <span className="text-xs text-slate-400">{place?.display_name}</span>
+      <h3 className="uppercase font-bold">{place?.name}</h3>
+      <p className="uppercase text-xs text-slate-600 text-center">Light rain</p>
+    </div>
+  );
+};
 
 export default Heading;
