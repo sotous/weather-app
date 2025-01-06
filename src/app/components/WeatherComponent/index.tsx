af80649068ff3555b48a81b300746e711968f3ce
Jesus Soto <sotous0@gmail.com>
2025-01-06 14:21:25 -0500

    Create weather component

---
+++ src/app/components/WeatherComponent/index.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Place } from "@/app/hooks/useSearch";
+
+type WeatherComponentProps = {
+  place: Place;
+};
+
+const WeatherComponent = ({ place }: WeatherComponentProps) => {};
+
+export default WeatherComponent;
