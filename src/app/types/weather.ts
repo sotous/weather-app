3e06db3cf7491df2fc7b0419e3259715384b4747
Jesus Soto <sotous0@gmail.com>
2025-01-21 18:31:17 -0500

    Create Weather type

---
+++ src/app/types/weather.ts
@@ -0,0 +1,25 @@
+export type Forecast = {
+  current: {
+    temperature_2m: number;
+    precipitation: number;
+    rain: number;
+    showers: number;
+    snowfall: number;
+    wind_speed_10m: number;
+    wind_direction_10m: number;
+    cloud_cover: number;
+    is_day: number;
+    sunshine_duration: number;
+    lightning_potential: number | null;
+  };
+  apparent_temperature_max: number[];
+  apparent_temperature_min: number[];
+};
+export type CurrentConditions = {
+  minTemp: number;
+  maxTemp: number;
+  currentTemp: number;
+  windSpeed: number;
+  windDirection: string;
+  condition: string;
+};
