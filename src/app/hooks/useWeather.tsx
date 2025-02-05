ef7653f3118fd231cd8fba95357317833b23c70f
Jesus Soto <sotous0@gmail.com>
2025-02-05 16:27:20 -0500

    Adjust params in the openMeteo API request

--- src/app/hooks/useWeather.tsx
+++ src/app/hooks/useWeather.tsx
@@ -8,7 +8,7 @@ const useWeather = () => {
      * 10m for Wind Speed, Wind Gusts and Wind Direction is set by default.
      * As 10m means that the API is delivering the wind data based on that altitude, 10 meters.
      */
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=apparent_temperature_max&daily=apparent_temperature_min&timezone=auto&current=temperature_2m&current=precipitation&current=rain&current=showers&current=snowfall&current=wind_speed_10m&current=wind_direction_10m&current=wind_gusts_10m&current=cloud_cover&current=is_day`;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=apparent_temperature_max&daily=apparent_temperature_min&timezone=auto&current=temperature_2m&current=wind_speed_10m&current=wind_direction_10m&current=wind_gusts_10m&current=cloud_cover&current=is_day&current=lightning_potential&current=cloud_cover_low&current=snowfall&current=weather_code`;
     let forecast = null;
     try {
       const response = await fetch(url);
@@ -20,16 +20,14 @@ const useWeather = () => {
       forecast = {
         current: {
           temperature_2m: data.current.temperature_2m,
-          precipitation: data.current.precipitation,
-          rain: data.current.rain,
-          showers: data.current.showers,
-          snowfall: data.current.snowfall,
           wind_speed_10m: data.current.wind_speed_10m,
           wind_direction_10m: data.current.wind_direction_10m,
           cloud_cover: data.current.cloud_cover,
+          cloud_cover_low: data.current.cloud_cover_low,
           is_day: data.current.is_day,
-          sunshine_duration: data.current.sunshine_duration,
+          weather_code: data.current.weather_code,
           lightning_potential: data.current.lightning_potential,
+          snowfall: data.current.snowfall,
         },
         apparent_temperature_max: data.daily.apparent_temperature_max,
         apparent_temperature_min: data.daily.apparent_temperature_min,
