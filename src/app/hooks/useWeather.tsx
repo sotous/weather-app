66e166cff171ea75d0c786a3651b5021e7a4df7e
Jesus Soto <sotous0@gmail.com>
2025-02-12 16:09:55 -0500

    Proper handle types to forecast and lighting potential

--- src/app/hooks/useWeather.tsx
+++ src/app/hooks/useWeather.tsx
@@ -10,7 +10,7 @@ const useWeather = () => {
      * Same would be for the temperature, which is delivered at 2 meters.
      */
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=apparent_temperature_max&daily=apparent_temperature_min&timezone=auto&current=temperature_2m&current=wind_speed_10m&current=wind_direction_10m&current=wind_gusts_10m&current=cloud_cover&current=is_day&current=lightning_potential&current=cloud_cover_low&current=snowfall&current=weather_code`;
-    let forecast = null;
+    let forecast: Forecast | null = null;
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -27,7 +27,7 @@ const useWeather = () => {
           cloud_cover_low: data.current.cloud_cover_low,
           is_day: data.current.is_day,
           weather_code: data.current.weather_code,
-          lightning_potential: data.current.lightning_potential,
+          lightning_potential: data.current.lightning_potential ?? 0,
           snowfall: data.current.snowfall,
         },
         apparent_temperature_max: data.daily.apparent_temperature_max,
