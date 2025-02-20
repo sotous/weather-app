23aff86501871b10069c774079086217d13c3703
Jesus Soto <sotous0@gmail.com>
2025-02-19 19:11:54 -0500

    Remove loader from header

--- src/app/components/Heading/index.tsx
+++ src/app/components/Heading/index.tsx
@@ -4,11 +4,10 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { Place } from "@/app/hooks/useSearch";
 
 type HeadingProps = {
-  place: Place;
-  setLoading: Dispatch<SetStateAction<boolean>>;
+  place: Place | null;
 };
 
-const Heading = ({ place, setLoading }: HeadingProps) => {
+const Heading = ({ place }: HeadingProps) => {
   return (
     <div className="pt-10 pb-[0.5rem] text-center">
       <span className="text-xs text-slate-400">{place?.display_name}</span>
